b3e0e96fa3734ed20a743dbe26babf5776d6dd45
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2015-03-07 17:07:19 -0300

    Adicionados controles para Drag'n'Drop
    !IMPORTANTE! --> Não crie um novo DragBox ou DragLabel, pois não seguirá
    os padrões para o funcionamento correto, copie e cole um existente!
    (Para criar um novo simplesmente copie e cole um existente).

--- GerenciadorDeHorarios/GerenciadorDeHorarios/Form1.Designer.cs
+++ GerenciadorDeHorarios/GerenciadorDeHorarios/Form1.Designer.cs
@@ -28,23 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dragBox1 = new DragBox.DragBox();
+            this.dragLabel1 = new DragBox.DragLabel();
             this.SuspendLayout();
+            // 
+            // dragBox1
+            // 
+            this.dragBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dragBox1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dragBox1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dragBox1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dragBox1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.dragBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dragBox1.ForeColor = System.Drawing.Color.White;
+            this.dragBox1.Location = new System.Drawing.Point(298, 122);
+            this.dragBox1.Name = "dragBox1";
+            this.dragBox1.Size = new System.Drawing.Size(113, 42);
+            this.dragBox1.TabIndex = 0;
+            this.dragBox1.UseVisualStyleBackColor = false;
+            // 
+            // dragLabel1
+            // 
+            this.dragLabel1.AutoSize = true;
+            this.dragLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.dragLabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dragLabel1.LastLocation = new System.Drawing.Point(0, 0);
+            this.dragLabel1.LinkedColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(196)))), ((int)(((byte)(13)))));
+            this.dragLabel1.Location = new System.Drawing.Point(416, 247);
+            this.dragLabel1.Name = "dragLabel1";
+            this.dragLabel1.NormalColor = System.Drawing.Color.Empty;
+            this.dragLabel1.Size = new System.Drawing.Size(104, 25);
+            this.dragLabel1.TabIndex = 1;
+            this.dragLabel1.Text = "Me Arraste";
+            this.dragLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.dragLabel1);
+            this.Controls.Add(this.dragBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gerenciador de Horários";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private DragBox.DragBox dragBox1;
+        private DragBox.DragLabel dragLabel1;
     }
 }
 
--- GerenciadorDeHorarios/GerenciadorDeHorarios/Form1.cs
+++ GerenciadorDeHorarios/GerenciadorDeHorarios/Form1.cs
@@ -7,14 +7,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace GerenciadorDeHorarios
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IMessageFilter
     {
+        //Drag'n'Drop required
+        System.Threading.Timer mouseUpTimer = null;
+        #region Mouse Hook Event
+        //Define os valores globais de Mouse.Pressed
+        //Drag'n'Drop required
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x201)
+                Mouse.Pressed = true;
+            else if (m.Msg == 0x202)
+                Mouse.Pressed = false;
+            return false;
+        }
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+            #region Mouse Hook
+            //Drag'n'Drop required
+            Application.AddMessageFilter(this);
+            this.FormClosed += delegate { Application.RemoveMessageFilter(this); };
+            #endregion
+            #region Adiciona os eventos de Drag'n'Drop
+            //Drag'n'Drop required
+            foreach (Control control in this.Controls)
+            {
+                if (control != null && control.GetType().ToString().Equals("DragBox.DragLabel"))
+                {
+                    DragBox.DragLabel dragLabel = (DragBox.DragLabel)control;
+                    dragLabel.MouseMove += Draggable_Hold;
+                    dragLabel.LastLocation = dragLabel.Location;
+                }
+            }
+            #endregion
         }
+
+        #region Drag'n'Drop
+        //Evento de quando um Controle é arrastado
+        private void Draggable_Hold(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //Define o Parent do Controle como a Form o "puxa" para frente e define sua posição
+                //para o mouse.
+                DragBox.DragLabel dragLabel = sender as DragBox.DragLabel;
+                if (dragLabel.Parent.GetType().ToString().Equals("DragBox.DragBox"))
+                    ((DragBox.DragBox)dragLabel.Parent).Empty = true;
+                dragLabel.AutoSize = true;
+                dragLabel.Linked = false;
+                dragLabel.Parent = this;
+                dragLabel.BringToFront();
+                dragLabel.Location = this.PointToClient(Cursor.Position);
+
+                //Inicia o método Mouse_Up com delay de 0.1s caso o mesmo seja nulo, ou reseta o timer
+                //caso já exista.
+                if (mouseUpTimer == null)
+                    mouseUpTimer = new System.Threading.Timer(obj => { Mouse_Up(dragLabel); }, null, 100, System.Threading.Timeout.Infinite);
+                else
+                    mouseUpTimer.Change(100, System.Threading.Timeout.Infinite);
+            }
+        }
+
+        //Método de quando um Controle arrastado é solto
+        public void Mouse_Up(DragBox.DragLabel dragLabel)
+        {
+            //Checa se o método foi chamado de maneira Thread-Safe e caso não tenha sido
+            //se chama novamente de maneira thread-safe.
+            if (this.InvokeRequired)
+            {
+                Invoke(new MethodInvoker(delegate()
+                {
+                    Mouse_Up(dragLabel);
+                }));
+            }
+            else
+            {
+                //Se o mouse estiver apertado reinicia o timer
+                if (Mouse.Pressed)
+                    mouseUpTimer.Change(100, System.Threading.Timeout.Infinite);
+                else
+                {
+                    //Define o timer para nulo, pega o controle que se encontra abaixo da posição do mouse e
+                    //"prende" o controle que estava sendo arrastado à ele.
+                    mouseUpTimer = null;
+                    Control box = this.GetChildAtPoint(this.PointToClient(new Point(Cursor.Position.X - 1, Cursor.Position.Y - 1)));
+                    if (box != null && box != dragLabel && box.GetType().ToString().Equals("DragBox.DragBox"))
+                    {
+                        DragBox.DragBox dragbox = (box as DragBox.DragBox);
+                        if (dragbox.Empty)
+                        {
+                            dragLabel.Parent = dragbox;
+                            dragbox.Empty = false;
+                            dragLabel.AutoSize = false;
+                            dragLabel.Width = dragLabel.Parent.Width;
+                            dragLabel.Height = dragLabel.Parent.Height;
+                            dragLabel.Linked = true;
+                            dragLabel.Location = new Point(0, 0);
+                        }
+                        else
+                        {
+                            //Caso o DragBox esteja cheio, o DragLabel é movido para sua última posição
+                            dragLabel.Location = dragLabel.LastLocation;
+                        }
+                    }
+                    else
+                    {
+                        //Caso o DragLabel não tenha sido arrastado para nenhum DragBox,
+                        //sua última posição é atualizada;
+                        dragLabel.LastLocation = this.PointToClient(Cursor.Position);
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
